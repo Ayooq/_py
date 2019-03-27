06db51e95e26b366f327fa7bfb9b96d8ccc0a892
Владислав <imtheinvictus1@gmail.com>
2019-03-27 21:53:53 +0600

    Delete api.py

--- app/api.py
+++
@@ -1,5 +0,0 @@
-'''URL components for Telegram bot.'''
-
-token = '551414683:AAE5YI8_oqZIDxkwvz3RTMys_cYwAfsUDA0'
-bot_api = 'https://api.telegram.org/bot' + token + '/'
-home = 'https://boriszblade.pythonanywhere.com/'
